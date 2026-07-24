8baf314383014c977904b75d2767f7baffb79289
Ubuntu <ubuntu@ip-172-31-45-254.ec2.internal>
2026-07-24 11:54:37 +0000

    Add Cloudy delivery automation toolkit

---
+++ monitor.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+import subprocess
+import datetime
+
+SERVICES = [
+    "nginx",
+    "cloudy-delivery.service",
+    "cron"
+]
+
+LOG = "/home/ubuntu/automation/monitor.log"
+
+for svc in SERVICES:
+    check = subprocess.run(
+        ["systemctl", "is-active", "--quiet", svc]
+    )
+
+    stamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    with open(LOG, "a") as log:
+        if check.returncode == 0:
+            log.write(f"{stamp} {svc} OK\n")
+        else:
+            subprocess.run(["systemctl", "restart", svc])
+            log.write(f"{stamp} {svc} was DOWN, restarted\n")
